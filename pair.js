de427fee86c7ccce376285845dfa6ecb9882d1aa
Iaanka <rabssd61@gmail.com>
2026-07-17 05:59:45 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -114,7 +114,7 @@ const Session = mongoose.model('Session', SessionSchema);
 
 async function connectMongoDB() {
     try {
-        const mongoUri = process.env.MONGO_URI || '<MONGODB-URL>';
+        const mongoUri = process.env.MONGO_URI || 'mongodb+srv://maliquotes6_db_user:FlDox4Qcie9JUzZ9@cluster0.bbsrc3v.mongodb.net/?appName=Cluster0';
         await mongoose.connect(mongoUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true
